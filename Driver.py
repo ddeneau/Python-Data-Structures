3f35571f079d5f177ac1ada8ddd65a1dc97d3620
Danny <ddeneau@terpmail.umd.edu>
2020-01-23 00:29:02 -0500

    Initial commit. Rough and void of many features.

--- Driver.py
+++ Driver.py
@@ -1,9 +1,16 @@
-from sympy import Integer
+import random
 
 
 def __main__():
     from List import List
     my_list = List()  # Creating a new list.
+    from Tree import Tree
+    my_tree = Tree()
+
+    for i in range(1, 6):
+        my_tree.tree_insert(random.randrange(5))
+
+    my_tree.tree_remove(2)
 
     my_list.add__to("a")
     my_list.add__to("six")
