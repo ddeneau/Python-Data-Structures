977d878e6866a507d68d663b91fa68152ffff6a0
Danny <ddeneau@terpmail.umd.edu>
2020-04-07 13:01:32 -0400

    BST Methods added.

--- Driver.py
+++ Driver.py
@@ -1,22 +1,23 @@
 import random
+from Tree import Tree
+from List import List
 
 
 def __main__():
-    from List import List
     my_list = List()  # Creating a new list.
-    from Tree import Tree
     my_tree = Tree()
 
-    for i in range(1, 6):
-        my_tree.tree_insert(random.randrange(5))
-
-    my_tree.tree_remove(2)
+    for i in range(1, 20):
+        my_tree.tree_insert(random.randrange(20))
 
     my_list.add__to("a")
     my_list.add__to("six")
     my_list.add__to("six")
 
     print(my_list.__str__())
+    print(my_tree.__str__())
+
+    my_tree.tree_remove(2)
 
 
 if __name__ == "__main__":
--- Tree.py
+++ Tree.py
@@ -1,16 +1,32 @@
-# An implementation of a Tree data structure
+# An implementation of a Tree (BST properties).
 from typing import Generic, TypeVar
 
-T = TypeVar('T')  # For Generics
+T = TypeVar('T')  # For placing any objects in our tree.
 
 
-# The basic unit of a Tree.
+# Node object to store data in units in the tree. Comparisons are based on the DATA field of each
+# node.
 class Node(Generic[T]):
 
+    # Constructor initializing a node with data. Left and right pointers are set to "None."
     def __init__(self, data: T):
         self.data = data
         self.left = None
         self.right = None
+
+    # Equals method comparing two nodes based on their DATA only.
+    def __eq__(self, other):
+        return self.data.__eq__(other.data)
+
+    # For debugging and printing. 
+    def __str__(self):
+        return "Node: " + self.data.__str__()
+
+    def __lt__(self, other):
+        return self.data < other.data
+
+    def __gt__(self, other):
+        return self.data > other.data
 
 
 # Recursive auxiliary method for adding data to a Tree.
@@ -32,26 +48,68 @@ def insert_helper(node, insertion):
 
 
 # Recursive auxiliary method for adding data to a Tree.
+# Base Case occurs when target node is found OR data is NoneType.
+# Otherwise, target node may be in left or right subtrees.
 def remove_helper(aux_node, removal):
-    if aux_node.data is removal:
-        if aux_node.right is None:
-            aux_node.data = aux_node.left.data
-            aux_node.left = aux_node.left.right
-        elif aux_node.left is None:
-            aux_node.data = aux_node.right.data
-            aux_node.right = aux_node.right.right
-        else:
-            aux_node.right = aux_node.right.right
-            aux_node.left = aux_node.left.right
+    if aux_node is None:
         return
+
+    if removal < aux_node:
+        aux_node.left = remove_helper(aux_node.left, removal)
+    elif removal > aux_node:
+        aux_node.right = remove_helper(aux_node.right, removal)
     else:
-        if aux_node.left is None:
-            remove_helper(aux_node.right, removal)
-        else:
-            remove_helper(aux_node.left, removal)
+        if aux_node.right is None:
+            return aux_node.left
+        elif aux_node.left is None:
+            return aux_node.right
+
+        temp_node = aux_node  # Create a temporary node in memory to keep track of target nodes references.
+        aux_node = find_minimum(temp_node.right)  # Set target node to be the minimum of its right subtree.
+        aux_node.right = remove_minimum_helper(temp_node.right)  # Corrects target references on right side
+        aux_node.left = temp_node.left  # Corrects target references on left side.
+
+    return aux_node
+
+
+def find_minimum(node):
+    if node.left is None:
+        return node
+
+    return find_minimum(node.left)
+
+
+def find_maximum(node):
+    if node.right is None:
+        return node
+
+    return find_maximum(node.right)
+
+
+def remove_minimum_helper(node):
+    if node.left is None:
+        return node.right
+
+    node.left = remove_minimum_helper(node.left)
+
+
+def remove_maximum_helper(node):
+    if node.right is None:
+        return node.left
+
+    node.right = remove_maximum_helper(node.right)
 
 
 # A Tree of Nodes. Does not contain duplicates.
+def string_aux(aux):
+    if aux is None:
+        return
+
+    string_aux(aux.left)
+    print(aux.__str__())
+    string_aux(aux.right)
+
+
 class Tree:
 
     def __init__(self):
@@ -67,3 +125,12 @@ class Tree:
 
     def tree_remove(self, data: T):
         remove_helper(self.root, data)
+
+    def __str__(self):
+        string_aux(self.root)
+
+    def remove_minimum(self):
+        remove_minimum_helper(self.root)
+
+    def remove_maximum(self):
+        remove_maximum_helper(self.root)
